ad8e16c13f2f9273a3bc85cd996cc52bcd961644
ANUJ BISHT <anujbisht09@gmail.com>
2023-03-24 08:20:19 +0530

    Update second_largest_element.py

--- Array/Second Largest Element/second_largest_element.py
+++ Array/Second Largest Element/second_largest_element.py
@@ -1,1 +1,24 @@
+# Program to print the second largest element in an array
+
+# Scan the total number of elements
+number_of_elements = int(input("Enter the number of elements of array: "))
+
+# Scan the elements of array
+print(f"Enter the {number_of_elements} elements of array separated by space: ", end="")
+array = list(map(int, input().split()))[:number_of_elements]
+
+# Sort the array
+array.sort()
+
+# Invert the sorted array
+invert_array = array[::-1]
+
+# Find the second largest element
+for i in range(len(array)):
+    if invert_array[i] < invert_array[0]:
+        second_max_element = invert_array[i]
+        break
+
+# Print the second largest elements
+print(f"Second largest Element: {second_max_element}")
 
