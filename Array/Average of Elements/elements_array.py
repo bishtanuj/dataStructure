d33ab2821af7140942884515e1081f440d9d9c86
ANUJ BISHT <anujbisht09@gmail.com>
2023-03-27 19:07:34 +0530

    Create elements_array.py

---
+++ Array/Average of Elements/elements_array.py
@@ -0,0 +1,16 @@
+# Program to find the average of the elements of an array
+if __name__ == '__main__':
+    # Scanning the number of elements of array
+    number_of_elements = int(input("Enter the total number of elements: "))
+
+    # Scanning the elements of array
+    array = list(map(int, input(f"Enter the {number_of_elements} elements: ").split()))[:number_of_elements]
+
+    # Finding the sum of elements of array
+    sum_of_elements = sum(array)
+
+    # Finding the average of elements of array
+    average_of_elements = sum_of_elements // number_of_elements
+
+    # Printing the average of elements
+    print(f"Average of elements: {average_of_elements}")
