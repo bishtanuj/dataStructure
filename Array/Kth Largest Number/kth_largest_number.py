05c89ae97822cfaf3aa491a79c4045a33e7a7384
ANUJ BISHT <anujbisht09@gmail.com>
2023-04-29 23:18:10 +0530

    Update kth_largest_number.py

--- Array/Kth Largest Number/kth_largest_number.py
+++ Array/Kth Largest Number/kth_largest_number.py
@@ -1,1 +1,17 @@
+# Program to find the largest kth element in array
+
+# function to find the largest kth element
+def find_kth_largest(nums: list[int], k: int) -> int:
+    nums.sort()
+    nums_new = nums[::-1]
+    return nums_new[k-1]
+
+
+# Driver function
+if __name__ == '__main__':
+    number_of_elements = int(input("Enter the number of elements: "))
+    array = list(map(int, input(f"Enter {number_of_elements} elements: ").split()))[:number_of_elements]
+    K = int(input("Enter the value of K: "))
+    result = find_kth_largest(array, K)
+    print(f"{K}th largest element: {result}")
 
