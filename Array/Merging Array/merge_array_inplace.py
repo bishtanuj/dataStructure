f95dcb93ebfd3802a5e5ed19c121075b0e5dc294
ANUJ BISHT <anujbisht09@gmail.com>
2023-05-05 17:14:23 +0530

    Update merge_array_inplace.py

--- Array/Merging Array/merge_array_inplace.py
+++ Array/Merging Array/merge_array_inplace.py
@@ -1,1 +1,19 @@
 # Program to merge two arrays without using extra space
+
+# function to merge two arrays
+def merge_array(arr1: list[int], arr2: list[int]) -> list[int]:
+    arr1.extend(arr2)
+    arr2.clear()
+    arr1.sort()
+    return arr1
+
+
+# driver function
+if __name__ == '__main__':
+    number1 = int(input("Enter the number of elements of array 1: "))
+    nums1 = list(map(int, input(f"Enter the {number1} elements: ").split()))
+    number2 = int(input("Enter the number of elements of array 2: "))
+    nums2 = list(map(int, input(f"Enter the {number2} elements: ").split()))
+    print("Array 1: ", nums1)
+    print("Array 2: ", nums2)
+    print(f"Merged Array: {merge_array(nums1, nums2)}")
