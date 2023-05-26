0a2fc0b247d0b5645db5bf3d768d3456c70abb79
ANUJ BISHT <anujbisht09@gmail.com>
2023-05-26 18:16:04 +0530

    Update single_element.py

--- Array/Single Element in Array/single_element.py
+++ Array/Single Element in Array/single_element.py
@@ -1,1 +1,20 @@
 # Program to find the single element in an array
+
+# function to find the single element
+def single_element(array: list[int]) -> int:
+    for i in range(len(array)):
+        singleElement = array.pop(i)
+        if singleElement in array:
+            array.insert(i, singleElement)
+            continue
+        else:
+            break
+    return singleElement
+
+
+# driver function
+if __name__ == '__main__':
+    element = int(input("Enter the number of elements of array: "))
+    elements_array = list(map(int, input(f"Enter {element} elements of array: ").split()))[:element]
+    print(f"Single element in array: {single_element(elements_array)}")
+
